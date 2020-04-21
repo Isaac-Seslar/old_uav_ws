678b6dbc928bf4c33af9b6e5a06b2212bc077f33
Bionic-sock <isaaciscool19@gmail.com>
2020-04-21 08:15:53 -0600

    CMakelist works

---
+++ build/uav_circle/CMakeFiles/uav_circle_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/uav_circle_node.dir/src/circle.cpp.o"
+  "/home/isaac/uav_ws/devel/lib/uav_circle/uav_circle_node.pdb"
+  "/home/isaac/uav_ws/devel/lib/uav_circle/uav_circle_node"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/uav_circle_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- build/uav_circle/catkin_generated/pkg.develspace.context.pc.py
+++ build/uav_circle/catkin_generated/pkg.develspace.context.pc.py
@@ -1,8 +1,8 @@
 # generated from catkin/cmake/template/pkg.context.pc.in
 CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/isaac/uav_ws/src/uav_circle/include".split(';') if "/home/isaac/uav_ws/src/uav_circle/include" != "" else []
+PROJECT_CATKIN_DEPENDS = "roscpp;rospy;std_msgs".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-luav_circle".split(';') if "-luav_circle" != "" else []
 PROJECT_NAME = "uav_circle"
 PROJECT_SPACE_DIR = "/home/isaac/uav_ws/devel"
 PROJECT_VERSION = "0.0.0"
